06dc5a4c4db992e990831dd31cf23ec36560e11e
Indy <indyli2002@yahoo.com.sg>
2026-07-11 04:30:45 +0800

    added  skip checkov again

--- main.tf
+++ main.tf
@@ -13,21 +13,19 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "indy-ce12-tfstate-12345"       # Change this
-    key    = "ce12-act3.2_terraform.tfstate" # Change this
+    bucket = "indy-ce12-tfstate-12345"
+    key    = "ce12-act3.2-terraform.tfstate"
     region = "ap-southeast-1"
   }
 }
 
-# checkov suppressions
-# checkov:skip=CKV_AWS_18
-# checkov:skip=CKV_AWS_144
-# checkov:skip=CKV_AWS_145
-# checkov:skip=CKV2_AWS_62
-# checkov:skip=CKV2_AWS_6
-# checkov:skip=CKV2_AWS_61
-# checkov:skip=CKV_AWS_21
-
+#checkov:skip=CKV_AWS_18:Access logging not needed for lab
+#checkov:skip=CKV_AWS_144:Cross-region replication not needed for lab
+#checkov:skip=CKV_AWS_145:KMS encryption not needed for lab
+#checkov:skip=CKV2_AWS_62:Event notifications not needed for lab
+#checkov:skip=CKV2_AWS_6:Public access block not needed for lab
+#checkov:skip=CKV2_AWS_61:Lifecycle config not needed for lab
+#checkov:skip=CKV_AWS_21:Versioning not needed for lab
 resource "aws_s3_bucket" "s3_tf" {
   bucket_prefix = "indy-demo-bucket-"
 
@@ -35,4 +33,3 @@ resource "aws_s3_bucket" "s3_tf" {
     Name = "CE12 Bucket"
   }
 }
-
