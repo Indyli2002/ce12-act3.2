bf8c2d44eb1f4d8d7983eb4ed5de57e62f5446ba
Indy <indyli2002@yahoo.com.sg>
2026-07-11 04:21:06 +0800

    added  skip checkov

--- main.tf
+++ main.tf
@@ -27,11 +27,11 @@ resource "aws_s3_bucket" "s3_tf" {
   }
 }
 
-  # checkov suppressions
-  # checkov:skip=CKV_AWS_18
-  # checkov:skip=CKV_AWS_144
-  # checkov:skip=CKV_AWS_145
-  # checkov:skip=CKV2_AWS_62
-  # checkov:skip=CKV2_AWS_6
-  # checkov:skip=CKV2_AWS_61
-  # checkov:skip=CKV_AWS_21
+# checkov suppressions
+# checkov:skip=CKV_AWS_18
+# checkov:skip=CKV_AWS_144
+# checkov:skip=CKV_AWS_145
+# checkov:skip=CKV2_AWS_62
+# checkov:skip=CKV2_AWS_6
+# checkov:skip=CKV2_AWS_61
+# checkov:skip=CKV_AWS_21
